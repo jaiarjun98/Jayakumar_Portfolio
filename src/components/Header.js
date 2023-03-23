b73b708a9783f273512c30af2b4b0176abd39666
Jayakumar <jaiarjun1998@gmaul.com>
2023-03-23 19:08:18 +0530

    Material UI package added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Projects from './Projects'
+import Projects from './Projects';
+import Button from '@mui/material/Button';
+
 const Header = () => {
     return (
         <div>
             My Header Function component called
+            <div><Button variant="contained">Hello World</Button></div>
             <Projects></Projects>
         </div>
     )
